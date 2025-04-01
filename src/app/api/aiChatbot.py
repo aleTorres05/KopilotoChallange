a0ca7d3b0d9d324345bd10653038fedfe2baa9df
Alejandro Torres <torresalexatlanta@gmail.com>
2025-04-01 01:41:59 -0600

    modified:   src/app/api/aiChatbot.py

--- src/app/api/aiChatbot.py
+++ src/app/api/aiChatbot.py
@@ -5,8 +5,9 @@ from dotenv import load_dotenv
 from services.conversation_service import get_conversation
 
 # context: id='
-async def call_ai(message, conv_id):
+async def answer_ai(message, conv_id):
     load_dotenv()
+    
     message_dict = {"content": message, "role": "user"}
     if conv_id:
         conversation = await get_conversation(conv_id)
@@ -16,17 +17,24 @@ async def call_ai(message, conv_id):
         ]
         new_conversation.append(message_dict)
         message_dict = new_conversation
+        ai_answer = call_ai(message_dict, message, conv_id)
+        return ai_answer
+        
 
     else:
+        message_dict["content"] = os.getenv("PROMPT") + message
         message_dict = [message_dict]
+        ai_answer = call_ai(message_dict, message, conv_id)
+        return ai_answer
+
+def call_ai(message_dict, message, conv_id):
     client = Groq(api_key=os.getenv("API_KEY"))
 
     chat_completion = client.chat.completions.create(
         messages=message_dict,
         model="llama-3.3-70b-versatile",
     )
-    # message_dict.append({"role": chat_completion.choices[0].message.role,
-    #                          "message": chat_completion.choices[0].message.content})
+
     messages = [
         {"role": "user", "message": message},
         {
